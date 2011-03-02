dc714848e1bc7758f30a88e827bea56de945c5c7
brainrape <brainrape@chaosmedia.hu>
2011-03-02 16:56:26 +0100

    test

--- settings.py
+++ settings.py
@@ -3,10 +3,14 @@
 this is the settings file for the lighttpd config generator
 """
 
+#required settings
 BASE_PATH='/opt/dp'
 DOMAIN='republicofpoker.com'
+
 ADMIN_MEDIA_PATH="/usr/share/pyshared/django/contrib/admin/media"
 
+
+#per-project settings
 EXTRA_CONFIG={
     'my_django_project':{
         'domain':r'^(www\.)?awesomeproject.com$'
